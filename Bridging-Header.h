a762df85fd8bf3c8dff1b39ade9152938b8aa0dd
Ben Gabay <bengabay89@gmail.com>
2015-07-09 15:22:58 -0700

    added facebook SDK and bridging header

---
+++ Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  Ramble
+//
+//  Created by Mac Admin on 7/9/15.
+//  Copyright (c) 2015 RMB. All rights reserved.
+//
+
+#ifndef Ramble_Bridging_Header_h
+#define Ramble_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+#endif
+
+
---
+++ Ramble/RMBUser.swift
@@ -0,0 +1,13 @@
+//
+//  RMBUser.swift
+//  Ramble
+//
+//  Created by Mac Admin on 7/9/15.
+//  Copyright (c) 2015 RMB. All rights reserved.
+//
+
+import Parse
+
+class RMBUser: PFUser {
+    
+}
